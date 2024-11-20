dc0ca5543c3d1719f3cd2daaa8984906cdd9c90a
Setu Shah <84060363+setu-shah@users.noreply.github.com>
2024-11-20 16:15:33 -0500

    Corrected source and variables

--- aws_project/genai/pipeline/deploy-ingest.py
+++ aws_project/genai/pipeline/deploy-ingest.py
@@ -2,12 +2,12 @@ from prefect import flow
 
 if __name__ == "__main__":
     flow.from_source(
-        source="https://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@github.com/ririsgrace/882-cloudshell-fall24.git",
+        source="https://github.com/ririsgrace/882-cloudshell-fall24.git",
         entrypoint="aws_project/genai/pipeline/flows/ingest-posts.py:job",
     ).deploy(
         name="genai-post-ingestion",
         work_pool_name="rgk-pool1",
-        job_variables={"env": {},
+        job_variables={"env": {"RGK": "loves-to-code"},
                        "pip_packages": ["pandas", "requests"]},
         cron="15 1 * * *",
         tags=["prod"],
