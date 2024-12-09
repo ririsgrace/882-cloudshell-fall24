ba24eb2c056b07d870658a33248e6e9d1b578cb4
ririsgrace <ririsgs@bu.edu>
2024-12-09 17:53:55 +0000

    delete data

--- trash/check_yfinance.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-
-# Fetch the data for the past 5 days
-ticker = "AAPL"
-new_data = yf.download(ticker, period="5d")
-
-# Display the data to check the actual dates
-print(new_data)
